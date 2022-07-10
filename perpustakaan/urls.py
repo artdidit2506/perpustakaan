7c54ef9a37d26a660762135bf7e7f8d899f3d73a
artdidit2506 <artdidit2506@gmail.com>
2022-07-10 21:31:17 +0700

    buat action

--- perpustakaan/urls.py
+++ perpustakaan/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.buku, name='buku'),
     path('penerbit/', views.penerbit, name='penerbit'),
-    path('tambah-buku/', views.tambah_buku, name='tambah-buku')
+    path('tambah-buku/', views.tambah_buku, name='tambah-buku'),
+    path('buku/ubah/<int:id_buku>', views.ubah_buku, name='ubah_buku'),
 ]
--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,8 +1,28 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perpustakaan.models import Buku
 from perpustakaan.forms import FormBuku
+from django.contrib import messages
 # Create your views here.
+
+
+def ubah_buku(request, id_buku):
+    buku = Buku.objects.get(id=id_buku)
+    if request.POST:
+        form = FormBuku(request.POST, instance=buku)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data berhasil diubah')
+            return redirect('ubah_buku', id_buku=id_buku)
+
+    else:
+        form = FormBuku(instance=buku)
+        context = {
+            'form': form,
+            'buku': buku,
+        }
+
+    return render(request, 'perpustakaan/ubah-buku.html', context)
 
 
 def buku(request):
